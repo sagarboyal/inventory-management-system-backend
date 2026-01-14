b77292b6e12f6ca93dc20f68656416dca7558eaf
Ritesh12u <roni.riteshghosh@gmail.com>
2026-01-14 12:33:22 +0530

    add Product Modules

--- src/main/java/com/app/main/product/payload/request/ProductRequest.java
+++ src/main/java/com/app/main/product/payload/request/ProductRequest.java
@@ -1,4 +1,14 @@
 package com.app.main.product.payload.request;
 
-public class ProductRequest {
+import java.math.BigDecimal;
+
+public record ProductRequest(String sku,
+                             String name,
+                             String description,
+                             String brand,
+                             BigDecimal price,
+                             String unit,
+                             Integer quantity,
+                             String imageUrl,
+                             Long categoryId) {
 }
